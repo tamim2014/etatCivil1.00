6113984312c2cec099a4eb5761b20e1adf591f6f
unknown <a.andjib@yahoo.fr>
2019-07-25 00:49:56 +0200

    largeurs OK, mais Hauteurs necessitent des MEDIA QUERY

--- accueil.php
+++ accueil.php
@@ -64,67 +64,45 @@ session_start();
 <html>
 <head>
  <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <!-- Forcer IE à utiliser le derier mode de compatibilité avec les autre navigateurs -->
-
  <meta charset="utf-8"> 
  <title> Accueil - menu de navigation</title>
  <script src="js/jquery.js"></script>
- 
 
-
-<link rel="stylesheet" type="text/css" href="css/accueil.css" />
-<link href="css/slide.css" rel="stylesheet" title="Style" />
-	<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	  <script src="assets/js/html5shiv.js"></script>
-	  <script src="assets/js/respond.min.js"></script>
-	<![endif]-->
-
- 
+ <link rel="stylesheet" type="text/css" href="css/accueil.css" />
+ <link href="css/slide.css" rel="stylesheet" title="Style" />
 
 </head>
 
 <body>
-<!-- ARRIERE PLAN :  -->
-<?php include("inc/accueil/accueil_background.php"); ?>
-
-
- <div class="centrer"  >
-<form action ="" method="POST" name="form1" >
-
-<!-- LE PANNEAU DE GAUCHE : Recher des document par numero ou nom -->
-
-  <table class="tablegauche" > <caption  style="caption-side:top;"> <font color="#FFFFFF"><h3> UNION DES COMORES  </h3><h6> Unit&eacute;-Solidarit&eacute;-D&eacute;veloppement  </h6><h4> MINISTERE DE L'INTERIEUR  </h4></font><img src="img/armoirie.png" style="z-index:3;"  /></caption>
-     <tr> <td>RECHERCHE DE DOCUMENT</td></tr>
-	 <tr><td> <font color="#cdbe9f"><b>Search by</b></font> num&eacute;ro<br/> <input id="recherchenum" type="text" name="acte_" pattern=".{1,}"  > </td></tr> 
-     <tr><td> <font color="#cdbe9f"><b>Search by</b></font> nom    <br/> <input id="recherchenom"  type="text" name="nom_"  > </td></tr>
-	 <tr><td>
-         <textarea class="t_area" name="myTextBox" cols="18" rows="4"> <?php echo $message ; ?> </textarea>
-     <br/><input type="submit" name="envoie" value="Chercher"  style="background-color: #e5eecc;"/>
-
-	 </td></tr>
-
- </table>
- 
-<!-- LE PANNEAU  CENTRAL : menu vertical (il serai + simple de mettre ce morceau de code dans un include  - voir le cours de Bezençon)-->
-
-
-
-<table  class="tablecentre"  ><caption style="caption-side:top;" > <font color="#FFFFFF"><h3> OFFICE   </h3></font></caption>
-
-	      <?php include("inc/accueil/accueil_menucentral.php"); ?>  
-</table>
-
-
-<!-- LE PANNEAU DE DROITE : Recher des document par liste déroulante -->
-
-<table  class="tabledroite" ><caption style="caption-side:top"> <font color="#FFFFFF"><h3> D'ETAT CIVIL  </h3></font></caption>
- <tr><td class="listemenu"> <?php include("inc/accueil/accueil_prefecture.php"); ?> </td></tr> 
- 
-</table>
-</form>
-
-</div>
-
+    <!-- ARRIERE PLAN   -->
+    <?php include("inc/accueil/accueil_background.php"); ?>
+	<!-- LES 3 PANNEAUX BLANCS  -->
+	<div class="centrer"  >
+		<form action ="" method="POST" name="form1" >
+			<!-- LE PANNEAU DE GAUCHE : Recher des document par numero ou nom -->
+				<table class="tablegauche" > <caption  style="caption-side:top;"> <font color="#FFFFFF"><h3> UNION DES COMORES  </h3><h6> Unit&eacute;-Solidarit&eacute;-D&eacute;veloppement  </h6><h4> MINISTERE DE L'INTERIEUR  </h4></font><img src="img/armoirie.png" style="z-index:3;"  /></caption>
+					<tr> <td>RECHERCHE DE DOCUMENT</td></tr>
+					<tr><td> <font color="#cdbe9f"><b>Search by</b></font> num&eacute;ro<br/> <input id="recherchenum" type="text" name="acte_" pattern=".{1,}"  > </td></tr> 
+					<tr><td> <font color="#cdbe9f"><b>Search by</b></font> nom    <br/> <input id="recherchenom"  type="text" name="nom_"  > </td></tr>
+					<tr>
+						<td>
+							<textarea class="t_area" name="myTextBox" cols="18" rows="4"> <?php echo $message ; ?> </textarea>
+							<br/>
+							<input type="submit" name="envoie" value="Chercher"  style="background-color: #e5eecc;"/>
+						</td>
+					</tr>
+				</table>			 
+			<!-- LE PANNEAU  CENTRAL : menu vertical (il serai + simple de mettre ce morceau de code dans un include  - voir le cours de Bezençon)-->
+			<table  class="tablecentre"  ><caption style="caption-side:top;" > <font color="#FFFFFF"><h3> OFFICE   </h3></font></caption>
+				<?php include("inc/accueil/accueil_menucentral.php"); ?>  
+			</table>
+			<!-- LE PANNEAU DE DROITE : Recher des document par liste déroulante -->
+			<table  class="tabledroite" ><caption style="caption-side:top"> <font color="#FFFFFF"><h3> D'ETAT CIVIL  </h3></font></caption>
+			   <tr><td class="listemenu"> <?php include("inc/accueil/accueil_prefecture.php"); ?> </td></tr> 
+			</table>
+		</form>
+		<div style="clear: both; float: none; height: 0;"></div>
+	</div>
 </body>
 </html>
 
--- inc/accueil/accueil_background.php
+++ inc/accueil/accueil_background.php
@@ -1,14 +1,15 @@
 <?php
  echo '
 <table class="closeUpperMargin"   ><tr><td>  </td></tr></table><!--rempli la marge du borne sup -->
-<table class="hollowTop"   >
-  <tr><td >  
-     <input type=image src="img/drapeau.png" align="left" class="flag" />
-  </td>
-  <td></td><td></td> 
-  <td style="padding-top:50px; color: #FFFFFF;" > <h3> OFFICE   </h3></td><td></td><td></td><td></td><td></td><td></td><td></td> 
-  <td style="padding-top:50px; color: #FFFFFF;"> <h3> D\'ETAT CIVIL  </h3></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td> 
-  </tr></table>  <!--  creux du haut -->
+
+<table class="hollowTop"   > <!--  creux du haut -->
+    <tr>  
+		<td ><input type=image src="img/drapeau.png" align="left" class="flag" /></td>
+		<td></td><td></td>  
+		<td style="padding-top:50px; color: #FFFFFF;" > <h3> OFFICE   </h3></td><td></td><td></td><td></td><td></td><td></td><td></td> 
+		<td style="padding-top:50px; color: #FFFFFF;"> <h3> D\'ETAT CIVIL  </h3></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td> 
+    </tr>
+</table>  
 <table class="hollowBottom"><tr><td>  </td></tr></table> <!--  creux du bas -->
 <table class="closeLowerMargin"     ><tr><td>  </td></tr></table> <!--  rempli les vide du milieu et la marge du borne inf -->
 <table class="line1"  style="z-index:3;"><tr><td>  </td></tr></table>
