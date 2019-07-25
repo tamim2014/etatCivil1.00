437901c0ac43419f42db3ab41f401a48b32c0e39
unknown <a.andjib@yahoo.fr>
2019-07-25 20:05:04 +0200

    ok en responsive 1920x1080px - il reste l'adaptation du text et de limage - ministere blabla ... et le cachet

--- inc/accueil/accueil_background.php
+++ inc/accueil/accueil_background.php
@@ -13,7 +13,7 @@
 <table class="hollowBottom"><tr><td>  </td></tr></table> <!--  creux du bas -->
 <table class="closeLowerMargin"     ><tr><td>  </td></tr></table> <!--  rempli les vide du milieu et la marge du borne inf -->
 <table class="line1"  style="z-index:3;"><tr><td>  </td></tr></table>
-<table class="line1"  style="z-index:3;  top:28.63%; "><tr><td>  </td></tr></table> 
+<table class="line2"  style="z-index:3; "><tr><td>  </td></tr></table> 
 <!--  <table class="line2"  style="z-index:3;"><tr><td>  </td></tr></table>  -->
 <table class="line3"   ><tr><td>  </td></tr></table>
 ';
