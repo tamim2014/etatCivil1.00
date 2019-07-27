b30d6e8016b4227076fe4cdae7f1fbfd88c5be32
unknown <a.andjib@yahoo.fr>
2019-07-27 16:41:34 +0200

    separer le style du menu centrale pour mieux le gerer . surtout sur les petit ecran

--- inc/accueil/accueil_menucentral.php
+++ inc/accueil/accueil_menucentral.php
@@ -2,47 +2,47 @@
  echo '
   
   <tr ><td > 
-  <p style="position:absolute; top:100px;" >
+  <p class="p1" >
     <a href="ecritureBD.php" class="nouveaudocument" > <input id="nouveaudocument" type="button" name="accueil" value="Nouvel Acte naissance"   ></a><br>
     <a href="lectureBD.php" class="listdocument" > <input  id="listdocument" type="button"    name="accueil" value="Liste Actes naissance"  ></a>
   </p>
 	 
-  <p style="position:absolute; top:168px;" > <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera -->
+  <p  class="p2" > <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera -->
     <a href="#" class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Marge de visibilié" style="color:#e5eecc;" ></a> <br>
 	 <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Marge de visibilié"  style="color:#e5eecc;"  ></a> 
   </p>
   
   
   
-  <p style="position:absolute; top:246px;"> 
+  <p  class="p3"> 
      <a href="#"  class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Nouvel Acte mariage" ></a> <br>
      <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Liste Acte mariage"  ></a> 
   </p>
   
   
-  <p style="position:absolute; top:300px;"> <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera --> 
+  <p  class="p4" > <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera --> 
      <a href="#" class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Marge de visibilié" style="color:#e5eecc;"  ></a> <br>
 	 <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Marge de visibilié"  style="color:#e5eecc;"    ></a> 
   </p>
 
  
-  <p style="position:absolute; top:356px;"> 
+  <p  class="p5" > 
 	 <a href="#" class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Nouvel Acte divorce"  ></a> <br> 
 	 <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Liste Acte divorce"  ></a> 
   </p>
  
   
-  <p style="position:absolute; top:414px;"> <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera -->
+  <p  class="p6"> <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera -->
      <a href="#" class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Marge de visibilié" style="color:#e5eecc;"  ></a> <br>
 	 <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Marge de visibilié"  style="color:#e5eecc;"    ></a> 
    </p> 
  
-  <p style="position:absolute; top:480px;"> 
+  <p  class="p7"> 
 	 <a href="#" class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Nouvel Acte dec&egrave;s" ></a> <br>     
 	 <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Liste Actes  dec&egrave;s"  ></a> 
    </p>
    
-  <p style="position:absolute; top:546px;"> <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera -->
+  <p  class="p8"> <!-- remplissage de caractere invisible pour serre la marge entre les 2 imput pour chrome et opera -->
      <a href="#" class="nouveaudocument"> <input id="nouveaudocument" type="button" name="accueil" value="Marge de visibilié" style="color:#e5eecc;"  ></a> <br>
 	 <a href="#" class="listdocument"> <input id="listdocument" type="button" name="accueil" value="Marge de visibilié"  style="color:#e5eecc;"    ></a> 
    </p> 
